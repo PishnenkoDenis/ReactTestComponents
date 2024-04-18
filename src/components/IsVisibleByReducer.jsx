27a683747192f45624f7319e5e9fb5471864a15d
d.pishnenko <d.pishnenko@tg.dunice.net>
2024-04-18 11:27:00 +0300

    Add component

---
+++ src/components/IsVisibleByReducer.jsx
@@ -0,0 +1,28 @@
+import React, { memo, useReducer } from "react";
+
+const Button = memo(({ onClick, children }) => {
+  return <button onClick={onClick}>{children}</button>;
+});
+
+export const IsVisibleReducer = () => {
+  //заменяет useState  и коллбэк, передаваемый в дочерний компонент стабильный
+  //Ссылка на него не меняется и перерндер дочернего компонента не происходит
+  //Заменяет useState и useCallback
+  const [isVisible, switchVisible] = useReducer((v) => !v, false);
+
+  const [count, increment] = useReducer((v) => v + 1, 0);
+
+  return (
+    <>
+      <p>{count}</p>
+      <Button onClick={increment}>Count</Button>
+      <Button onClick={switchVisible}>Click</Button>
+      {isVisible && (
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime,
+          necessitatibus.
+        </p>
+      )}
+    </>
+  );
+};
